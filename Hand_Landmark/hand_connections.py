2ae7d207d794c0640954eae7066940354ff91eca
Sanaullah <alamsanaullah57@gmail.com>
2026-02-28 22:22:06 +0530

    Depth Based(grey scaled line)  HandLandmark connection

--- Hand_Landmark/hand_connections.py
+++ Hand_Landmark/hand_connections.py
@@ -6,7 +6,7 @@ from mediapipe.tasks.python import vision
 from mediapipe.tasks.python.vision import HandLandmarksConnections
 
 def Hand_Landmark_Connections():
-  model_path = "hand_landmarker.task"
+  model_path = "Hand_Landmark/hand_landmarker.task"
 
   # callback function
   detection_result = None
@@ -34,7 +34,7 @@ def Hand_Landmark_Connections():
       if not ret:
         break
       # resize frame
-      frame = cv2.resize(frame, (1200, 900))
+      # frame = cv2.resize(frame, (1200, 900))
       # flip the frame horizontally
       frame = cv2.flip(frame, 1)
       # BGR(frame by cv2) to RGB (mediapipe accepts RGB image)
@@ -48,12 +48,21 @@ def Hand_Landmark_Connections():
         h, w, _ = frame.shape
 
         for hand_landmarks in detection_result.hand_landmarks: #type: ignore
-          # Land mark points
+
+          # Collect z values for normalization
+          z_values = [lm.z for lm in hand_landmarks]
+          z_min = min(z_values)
+          z_max = max(z_values)
+
+          # Avoid division by zero
+          z_range = z_max - z_min if z_max - z_min != 0 else 1
+
+          # Draw landmarks (BLACK circles)
           for lm in hand_landmarks:
             cx, cy = int(lm.x * w), int(lm.y * h)
-            cv2.circle(frame, (cx, cy), 5, (162, 217, 188), -1)
+            cv2.circle(frame, (cx, cy), 5, (0, 0, 0), -1)
 
-          # Connect Landmarks
+          # Draw depth-based grey connections
           for connection in HandLandmarksConnections.HAND_CONNECTIONS:
             start = hand_landmarks[connection.start]
             end = hand_landmarks[connection.end]
@@ -61,7 +70,28 @@ def Hand_Landmark_Connections():
             x1, y1 = int(start.x * w), int(start.y * h)
             x2, y2 = int(end.x * w), int(end.y * h)
 
-            cv2.line(frame, (x1, y1), (x2, y2), (148, 161, 68), 2)
+            # Average depth of the connection
+            avg_z = (start.z + end.z) / 2
+
+            # Normalize depth → 0 to 1
+            norm_z = (avg_z - z_min) / z_range
+
+            # Map to grey intensity (closer = lighter)
+            grey = int(255 * (1 - norm_z*1.2))
+
+            cv2.line(frame, (x1, y1), (x2, y2), (grey, grey, grey), 2)
+
+      # upscale frame (frame drops by 3-4 -depends on system)
+      def Upscale_and_Resize_Frame(frame):
+        # Scale image up by 2x using high-quality interpolation
+        width = int(frame.shape[1] * 2)
+        height = int(frame .shape[0] * 2)
+        dim = (width, height)
+
+        # INTER_CUBIC is slower but provides better results for upscaling
+        resized_img = cv2.resize(frame, dim, interpolation=cv2.INTER_CUBIC)
+        return resized_img
+      frame = Upscale_and_Resize_Frame(frame)
 
       def display_FPS_on_frame(frame, position=(10, 50),font=cv2.FONT_HERSHEY_SIMPLEX, scale=1, color=(70, 70, 191), thickness=2, line_type=cv2.LINE_AA):
         # FPS calculation
@@ -71,9 +101,7 @@ def Hand_Landmark_Connections():
         pTime = cTime
 
         # Display FPS on frame
-
         cv2.putText(frame, f"FPS: {fps}", position, font, scale, color, thickness, line_type)
-
       display_FPS_on_frame(frame)
 
       cv2.imshow("Hand Tracking", frame)
